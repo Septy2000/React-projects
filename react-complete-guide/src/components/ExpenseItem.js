f7c4e3fa101b05744fe7aa1415bb608311e596fc
Septy2000 <calin.septimiu28@gmail.com>
2023-01-23 20:33:05 +0000

    added variables to component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem () {
+    const expenseDate = new Date(2021, 2, 28).toDateString();
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 295.67;
+
+
     return (
         <div className='expense-item'>
-            <div>March 28th 2021</div>
+            <div>{expenseDate}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$249.99</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>
         </div>
     );
