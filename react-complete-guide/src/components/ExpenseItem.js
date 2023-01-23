f16bffbf50038abcb2e7a40ff5047fae186bcc41
Septy2000 <calin.septimiu28@gmail.com>
2023-01-23 21:19:50 +0000

    added date components

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,23 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
 
 function ExpenseItem(props) {
-    const expenseDate = props.date;
-    const expenseTitle = props.title;
-    const expenseAmount = props.amount;
-
-    const month = expenseDate.toLocaleString('en-US', {month: 'long'});
-    const year = expenseDate.getFullYear();
-    const day = expenseDate.toLocaleString('en-US', {day: '2-digit'});
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
